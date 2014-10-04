41d446b3050da048e2f7756fa7e17f8e0d6e5db6
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2014-10-04 17:06:25 +0100

    added sqlite3 adapter

---
+++ lib/adapters/sqlite3.rb
@@ -0,0 +1,21 @@
+require 'sqlite3'
+
+class Sqlite3
+
+  def initialize(database,show_sql=false)
+    @client =  Sqlite3::Database.new(database)
+    @show_sql = show_sql
+  end
+
+  def query(sql)
+    puts sql if @show_sql
+    @client.execute(sql)
+  end
+
+  def fields(table)
+    query('select * from ' + table.to_s).fields
+  end
+
+end
+
+
